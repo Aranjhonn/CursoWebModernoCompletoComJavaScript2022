0e033bd6ffbe7ae011a434a13383c7850c8bc1b4
Aranjhonn <aranjhonn@yahoo.com>
2022-08-05 21:20:16 -0300

    Testando Temporizadores

---
+++ exercicios-js/node/temporizador.js
@@ -0,0 +1,25 @@
+// Executando tarefa de 5 em 5 segundos
+const schedule = require('node-schedule')
+
+const tarefa1 = schedule.scheduleJob('*/5 * * * * *', function() {
+    console.log('Executando tarefa 1!', new Date().getSeconds())
+})
+
+// Cancelando a tarefa
+setTimeout(function() {
+    tarefa1.cancel()
+    console.log('Cancelando Tarefa 1!')
+}, 20000)
+
+// Outros temporizadores:
+// setImmediate
+// setInterval
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1, 5)]
+regra.hour = 12
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function() {
+    console.log('Executando tarefa 2!', new Date().getSeconds())
+})
